9ecde5c54430d65e31030c65636c349338837f7b
dcharkes <dc.harkes@gmail.com>
2013-10-28 16:03:09 +0100

    Connection with a database

--- Recursive-Aggregation-MsSql/Program.cs
+++ Recursive-Aggregation-MsSql/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Recursive_Aggregation_MsSql.Model;
 
 namespace Recursive_Aggregation_MsSql
 {
@@ -10,6 +11,19 @@ namespace Recursive_Aggregation_MsSql
     {
         static void Main(string[] args)
         {
+            GradesDataContext gradesDb = new GradesDataContext();
+
+            var s = new Student();
+            gradesDb.Students.InsertOnSubmit(s);
+            gradesDb.SubmitChanges();
+            Console.WriteLine(s.id);
+
+            foreach (var student in from stud in gradesDb.Students select stud)
+            {
+                Console.WriteLine("Found a student, id = {0}.", student.id);
+            }
+
+            Console.ReadKey();
         }
     }
 }
